e4652887af30e1c61eeec30b93190017e4294af8
Anita C <Anita C>
2016-05-05 09:18:57 -0700

    graphing subplots

--- trial4.m
+++ trial4.m
@@ -1,36 +1,56 @@
 function line1 = trial4()
 h.fig = figure('position', [200 200 800 400]);
 h.buttonOne = uicontrol('style', 'pushbutton',...
-    'position', [550 325 50 50],...
-    'string', 'E');
+    'position', [650 325 50 50],...
+    'string', 'Endo');
 
-ax1 = subplot(1, 2, 1);
-ax2 = subplot(1, 2, 1);
-line1 = line(ax1, NaN, NaN, 'Color', 'Red');
+h.buttonTwo = uicontrol('style', 'pushbutton',...
+    'position', [650 250 50 50],...
+    'string', 'TOF');
+
+h.buttonThree = uicontrol('style', 'pushbutton',...
+    'position', [650 175 50 50],...
+    'string', 'Fade');
+
+h.buttonFour = uicontrol('style', 'pushbutton',...
+    'position', [650 100 50 50],...
+    'string', 'Recovery');
+
+h.buttonFive = uicontrol('style', 'pushbutton',...
+    'position', [650 25 50 50],...
+    'string', 'Extubation');
+
+ax1 = subplot('Position', [0.1 0.15 0.6 0.75]);
+ax2 = subplot('Position', [0.1 0.15 0.6 0.75]);
+h.line1 = line(ax1, NaN, NaN, 'Color', 'Red');
 x = 0;
 while 1
+    title('EMAP');
+    xlabel('Time');
+    ylabel('Pressure [mmHg]');
     x = x + 1;
     y = sin(x);
-    x1 = get(line1, 'xData');
-    y1 = get(line1, 'yData');
+    x1 = get(h.line1, 'xData');
+    y1 = get(h.line1, 'yData');
     x1 = [x1 x];
     y1 = [y1 y];
-    set(line1, 'xData', x1, 'yData', y1);
+    set(h.line1, 'xData', x1, 'yData', y1);
     hold on;
-    scatter(ax2, x, y, 'MarkerFaceColor', 'red');
+%     press;
+    %scatter(ax2, x, y, 'MarkerFaceColor', 'blue');
 
     pause(0.1);
     axis([x - 100 x + 100 -1.5 1.5]);
-    
-    %n = get(gca, 'CurrentPoint');
-    %text(newpoint(1) + 100,sin(newpoint(1)), num2str(newpoint(1)));
-    set(h.buttonOne, 'callback','press(a,sin(a))');
+    hold on;
+    set(h.buttonOne, 'callback', @press);
     
 end
 
-    function press(i, j)
+    function press(hObject, callbackdata);
         hold on;
-        scatter(i, j);
+%         ax2 = subplot('Position', [0.1 0.15 0.6 0.75]);
+%         a = hObject.get(gca, 'CurrentPoint');
+%         b = sin(a);
+        scatter(ax2, x, y, 'MarkerFaceColor', 'blue');
     end
-
 end
